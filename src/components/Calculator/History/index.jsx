45fa966e472de3d1c6f5603c42508df08c42dc04
DRON <aka.cc@mail.ru>
2021-08-03 15:21:49 +0300

    feat: update History component
    add styling component

---
+++ src/components/Calculator/History/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Heading} from "@/pages/Home/components"
+
+
+export const History = () => {
+    return (
+      <div>
+        <Heading>History</Heading>
+        <textarea>History textarea</textarea>
+      </div>
+    )
+}
