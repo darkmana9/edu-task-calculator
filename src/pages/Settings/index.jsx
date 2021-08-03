d07f770fc711f701ad49cab6938d3a87dbbd86b1
DRON <aka.cc@mail.ru>
2021-08-03 18:40:44 +0300

    feat: add Settings page and routing

---
+++ src/pages/Settings/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { PageLayout } from '@/layouts'
+import {Calculator} from "@/components/Calculator"
+
+export default () => {
+  return (
+    <PageLayout>
+      <Calculator/>
+    </PageLayout>
+  )
+}
