2471e05335a5d07756d954369c97a7d70a7d94a6
DRON <aka.cc@mail.ru>
2021-08-03 15:56:56 +0300

    feat: add styling for Header
    feat: add Header title text
    
    with styling

---
+++ src/components/Header/components.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const HeaderTitleText = styled.span`
+  font-size: 50px;
+`
+
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import {NavLink} from "react-router-dom"
+import {HeaderTitleText} from "@/components/Header/components"
 
 export const Header = () => {
-    return (
-      <div>
-        <div>
-          Calculator App
-        </div>
-        <nav>
-<NavLink to="/home">Home</NavLink>
-<NavLink to="/settings">Settings</NavLink>
-        </nav>
-      </div>
-    )
+  return (
+    <div>
+      <HeaderTitleText>
+        Calculator App
+      </HeaderTitleText>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/settings">Settings</NavLink>
+      </nav>
+    </div>
+  )
 }
