7ac7933127e525d6cf492d47e1bbf5e8df3a679c
DRON <aka.cc@mail.ru>
2021-08-03 14:09:02 +0300

    fix!: remove Heading and Card from Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,9 +8,7 @@ import { Card, Heading } from './components'
 export default () => {
   return (
     <PageLayout>
-      <Card>
-        <Heading id="welcome">Welcome!</Heading>
-      </Card>
+
     </PageLayout>
   )
 }
