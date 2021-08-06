6e2edca6f70a19711784012d8e4e68e3e1605774
DRON <aka.cc@mail.ru>
2021-08-06 15:31:21 +0300

    feat: add error boundary for Calculator

--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -4,6 +4,38 @@ import {History} from "@/components/Calculator/History"
 import {Keypad} from "@/components/Calculator/Keypad"
 import {CalculatorLayout} from "@/layouts"
 
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+      super(props)
+
+      this.state = {
+        hasError: false,
+        error: '',
+        errorInfo: '',
+      }
+    }
+
+    static getDerivedStateFromError(){
+        return {
+          hasError: true,
+        }
+    }
+
+    componentDidCatch(error, errorInfo) {
+        this.setState({
+          error: error,
+          errorInfo: errorInfo,
+        })
+    }
+
+    render() {
+      if(this.state.hasError){
+        return <h1>Error: {this.state.error}, error info: {this.state.errorInfo}</h1>
+      }
+      return this.props.children
+    }
+
+}
 
 export class Calculator extends React.Component{
   constructor(props) {
@@ -24,9 +56,11 @@ export class Calculator extends React.Component{
   render() {
     return (
       <CalculatorLayout>
+        <ErrorBoundary>
         <Display inputValue={this.state.inputValue}/>
         <History/>
         <Keypad handleNumbersButtons={this.handleNumbersButtons}/>
+        </ErrorBoundary>
       </CalculatorLayout>
     )
   }
