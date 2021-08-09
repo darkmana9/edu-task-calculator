047a878f518d531af87bf9c9885b1a64a5bc2f16
DRON <aka.cc@mail.ru>
2021-08-09 15:44:11 +0300

    fix: move ErrorBoundary outside Calculator

--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -4,38 +4,7 @@ import {History} from "@/components/Calculator/History"
 import {Keypad} from "@/components/Calculator/Keypad"
 import {CalculatorLayout} from "@/layouts"
 
-class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props)
 
-    this.state = {
-      hasError: false,
-      error: '',
-      errorInfo: '',
-    }
-  }
-
-  static getDerivedStateFromError() {
-    return {
-      hasError: true,
-    }
-  }
-
-  componentDidCatch(error, errorInfo) {
-    this.setState({
-      error: error,
-      errorInfo: errorInfo,
-    })
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return <h1>Error: {this.state.error}, error info: {this.state.errorInfo}</h1>
-    }
-    return this.props.children
-  }
-
-}
 
 function add(x, y) {
   return Math.round((x + y) * 1000) / 1000
@@ -208,13 +177,13 @@ export class Calculator extends React.Component {
   render() {
     return (
       <CalculatorLayout>
-        <ErrorBoundary>
+
           <Display inputValue={this.state.inputValue}/>
           <History history={this.state.history}/>
 
           <Keypad handleSimpleOperationsButton={this.handleSimpleOperationsButton} handleOperationsButton={this.handleOperationsButton}
                   handleNumbersButtons={this.handleNumbersButtons}/>
-        </ErrorBoundary>
+
       </CalculatorLayout>
     )
   }
